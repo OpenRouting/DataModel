cf839f9a2c78f6bcaa302d9a795fcc61051755a1
Christopher Frickd <christopher.fricke@bmoregeo.com>
2015-02-20 21:33:01 -0500

    Create function to locate points

---
+++ get_waypoint.sql
@@ -0,0 +1,8 @@
+-- Locate user defined point on network
+-- Author: Christopher Fricke
+-- License: MIT
+
+-- DROP FUNCTION routing.get_waypoint(geometry);
+CREATE OR REPLACE FUNCTION routing.get_waypoint(test_geom geometry) RETURNS int AS $$
+    SELECT id::integer FROM routing.ways_vertices_pgr ORDER BY the_geom <-> test_geom LIMIT 1;;
+$$ LANGUAGE SQL;
