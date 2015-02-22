b685b67f932683192acb9c84a99c321ec429a482
Christopher Frickd <christopher.fricke@bmoregeo.com>
2015-02-22 13:24:17 -0500

    Update view_way_vertices.sql

--- view_way_vertices.sql
+++ view_way_vertices.sql
@@ -1,5 +1,19 @@
-SELECT ways_vertices_pgr.*
-FROM 
-	routing.ways_vertices_pgr,
-	routing.ways
-WHERE ways_vertices_pgr.id in (ways.source, ways.target)
+-- View: routing.way_vertices
+
+-- DROP VIEW routing.way_vertices;
+
+CREATE OR REPLACE VIEW routing.way_vertices AS 
+ SELECT DISTINCT ways_vertices_pgr.id,
+    ways_vertices_pgr.cnt,
+    ways_vertices_pgr.chk,
+    ways_vertices_pgr.ein,
+    ways_vertices_pgr.eout,
+    ways_vertices_pgr.the_geom
+   FROM routing.ways_vertices_pgr,
+    routing.ways_with_info
+  WHERE ways_vertices_pgr.id = ways_with_info.target OR ways_vertices_pgr.id = ways_with_info.source;
+
+ALTER TABLE routing.way_vertices
+  OWNER TO postgres;
+GRANT ALL ON TABLE routing.way_vertices TO postgres;
+GRANT SELECT ON TABLE routing.way_vertices TO readonly;
