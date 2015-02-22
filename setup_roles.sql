7a721acae59173556cc18a284e28d1aa159d7c32
Christopher Frickd <christopher.fricke@bmoregeo.com>
2015-02-22 09:59:30 -0500

    Update setup_roles.sql

--- setup_roles.sql
+++ setup_roles.sql
@@ -1,6 +1,11 @@
 CREATE ROLE readonly;
+GRANT USAGE ON SCHEMA public TO readonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
+
+GRANT USAGE ON SCHEMA routing TO readonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA routing TO readonly;
+
+GRANT USAGE ON SCHEMA osm TO readonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA osm TO readonly;
 BEGIN;
 CREATE ROLE "geoservice" WITH LOGIN ENCRYPTED PASSWORD 'geoservice' IN ROLE readonly;
