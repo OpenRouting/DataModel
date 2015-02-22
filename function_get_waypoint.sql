bcc6b13e0695e3f1c4c7bf85c5c5a354479d6efb
Christopher Frickd <christopher.fricke@bmoregeo.com>
2015-02-22 10:51:16 -0500

    use way_vertices view

--- function_get_waypoint.sql
+++ function_get_waypoint.sql
@@ -4,5 +4,5 @@
 
 -- DROP FUNCTION routing.get_waypoint(geometry);
 CREATE OR REPLACE FUNCTION routing.get_waypoint(test_geom geometry) RETURNS int AS $$
-    SELECT id::integer FROM routing.ways_vertices_pgr ORDER BY the_geom <-> test_geom LIMIT 1;;
+    SELECT id::integer FROM routing.way_vertices ORDER BY the_geom <-> test_geom LIMIT 1;;
 $$ LANGUAGE SQL;
